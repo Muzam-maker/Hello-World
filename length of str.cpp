99056ea2b21dc7fdbf1f459331c5b836bf444cb5
Muzamil <87494541+Zamil263@users.noreply.github.com>
2021-07-19 19:44:42 +0000

    Add files via upload
    To find Length of string

---
+++ length of str.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    s="Hello";
+    cout<<s.length()<<endl;
+    cout<<s.size()<<endl; //Both are same ways
+    
+    return 0;
+
+    }
